26b305cec8c4895aeec3f7e894fd1a3005cdfeeb
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-16 11:51:29 +0200

    added name field to door class

--- Door.java
+++ Door.java
@@ -1,10 +1,12 @@
 public class Door {
   String id;
+  String name;
   String roomId1;
   String roomId2;
 
-  public Door(String id, String roomId1, String roomId2) {
+  public Door(String id, String name, String roomId1, String roomId2) {
     this.id = id;
+    this.name = name;
     this.roomId1 = roomId1;
     this.roomId2 = roomId2;
   }
