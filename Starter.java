522256a571eea4ecd7e311d119b7100d7e4b27ed
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-16 09:45:24 +0200

    created basic structure for starter class

--- Starter.java
+++ Starter.java
@@ -1,5 +1,11 @@
 public class Starter {
+  public static boolean gameOver = false;
   public static void main(String[] args) {
-    
+    Map map = MapArchive.TESTING_MAP;
+    Controller controller = new Controller(map,"not_a_room");
+    do {
+      controller.printCurrentRoom();
+      controller.getInput();
+    } while (!gameOver);
   }
 }
