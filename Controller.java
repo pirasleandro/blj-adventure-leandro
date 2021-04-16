8fdca03d3fbbe393d52c80cd7188e63d42fc349d
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-16 09:42:41 +0200

    created basic structure for controller class
    this class has the methods that the starter class will loop through. the player decides wether they want to inspect or leave the room. inspectRoom() and leaveRoom() not implemented yet

--- Controller.java
+++ Controller.java
@@ -1,3 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Controller {
-  Map map = new Map();
+  private Map map;
+  String currentRoomId;
+
+  public Controller(Map map, String starterRoomId) {
+    this.map = map;
+    this.currentRoomId = starterRoomId;
+  }
+
+  public void printCurrentRoom() {
+    map.getRoom(currentRoomId).print();
+  }
+
+  public void getInput() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("[1] = inspect room");
+    System.out.println("[2] = leave room");
+    String input = scan.nextLine();
+    switch (input) {
+      case "1" -> {
+        //todo
+      }
+      case "2" -> {
+        leaveRoom();
+      }
+    }
+  }
+
+  private void leaveRoom() {
+    ArrayList<String> awailableDoors = map.getDoorsOfRoom(currentRoomId);
+  }
 }
