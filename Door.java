44f855ef98e1707a675433630f430b920148c751
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-16 09:34:01 +0200

    created basic structure for Door class

--- Door.java
+++ Door.java
@@ -1,3 +1,11 @@
 public class Door {
-  
+  String id;
+  String roomId1;
+  String roomId2;
+
+  public Door(String id, String roomId1, String roomId2) {
+    this.id = id;
+    this.roomId1 = roomId1;
+    this.roomId2 = roomId2;
+  }
 }
