b25b90463192c137bbfc3f81f5b357eba230c07f
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-16 11:51:59 +0200

    edited testing map with box drawing characters

--- MapArchive.java
+++ MapArchive.java
@@ -1,4 +1,7 @@
+import boxdrawing.*;
+
 public class MapArchive {
+
   private MapArchive() {
     // static class
   }
@@ -6,28 +9,31 @@ public class MapArchive {
   public static final Map TESTING_MAP = new Map(
     new Room[] {
       new Room("starter_room", "Starter Room",
-        "+-----------+\n" +
-        "|   STARTER |\n" + 
-        "|           a\n" +
-        "+------ b --+"
+        "┌─────────┐\n" +
+        "│         │\n" +
+        "│         " + ASCIIart.verticalDoor(AnsiColors.BLUE) + "\n" +
+        "│         │\n" +
+        "└───" + ASCIIart.horizontalDoor(AnsiColors.RED) + "───┘"
       ),
       new Room("second_room", "Second Room",
-        "+----------+\n" +
-        "|   SECOND |\n" + 
-        "a          |\n" +
-        "+-- b -----+"
+        "┌─────────┐\n" +
+        "│         │\n" +
+        ASCIIart.verticalDoor(AnsiColors.BLUE) + "         │\n" +
+        "│         │\n" +
+        "└───" + ASCIIart.horizontalDoor(AnsiColors.GREEN) + "───┘"
       ),
       new Room("third_room", "Third Room",
-        "+-- a -------- b --+\n" +
-        "|   THIRD ROOM     |\n" + 
-        "|                  |\n" +
-        "+------------------+"
+        "┌───" + ASCIIart.horizontalDoor(AnsiColors.RED) + "────────" + ASCIIart.horizontalDoor(AnsiColors.GREEN) + "───┐\n" + 
+        "│                    │\n" +
+        "│                    │\n" +
+        "│                    │\n" +
+        "└────────────────────┘"
       )
     },
     new Door[] {
-      new Door("start_second", "starter_room", "second_room"),
-      new Door("start_third", "starter_room", "third_room"),
-      new Door("second_third", "second_room", "third_room")
+      new Door("start_second", "Blue Door", "starter_room", "second_room"),
+      new Door("start_third", "Red Door", "starter_room", "third_room"),
+      new Door("second_third", "Green Door", "second_room", "third_room")
     }
     );
 }
