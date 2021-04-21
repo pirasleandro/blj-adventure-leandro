40357943c00eedb1f0a106dc70e5a75a7186d7c3
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-21 09:06:09 +0200

    added isLocked boolean
    used to lock a door, so it can be unlocked with a key item later

--- Door.java
+++ Door.java
@@ -1,13 +1,15 @@
 public class Door {
-  String id;
-  String name;
-  String roomId1;
-  String roomId2;
+  public String id;
+  public String name;
+  public String roomId1;
+  public String roomId2;
+  public boolean isLocked;
 
-  public Door(String id, String name, String roomId1, String roomId2) {
+  public Door(String id, String name, String roomId1, String roomId2, boolean isLocked) {
     this.id = id;
     this.name = name;
     this.roomId1 = roomId1;
     this.roomId2 = roomId2;
+    this.isLocked = isLocked;
   }
 }
