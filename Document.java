c6a2bf0b0f2289d64aa60d00fdf9ae6901047ff1
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-21 09:04:44 +0200

    added document class
    player will be able to collect them to get information about the anomalies in the game

---
+++ Document.java
@@ -0,0 +1,26 @@
+public class Document extends Item {
+
+  public Document(String id, String name, String description) {
+    super(id, name, description);
+    //TODO Auto-generated constructor stub
+  }
+
+  @Override
+  void use() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  void take() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  void drop() {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
