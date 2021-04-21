e0bf1493644807d66a7eeaaa5674ba76dcabacf4
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-21 09:03:16 +0200

    added a way for maps to link to eachother
    the goal is to have three maps:
    ENTRANCE_ZONE
    LIGHT_CONTAINMENT
    HEAVY_CONTAINMENT
    linked to eachother by start_room and end_room

--- Starter.java
+++ Starter.java
@@ -1,13 +1,18 @@
 public class Starter {
   public static boolean gameOver = false;
+  public static int mapStage = 1;
   public static void main(String[] args) {
-    Map map = MapArchive.TESTING_MAP;
+    Map map = MapArchive.ENTRANCE_ZONE;
     Player player = new Player("testing");
     Controller controller = new Controller(map,player,"starter_room");
     do {
       ASCIIart.clearConsole();
       controller.printCurrentRoom();
       controller.getInput();
+      if (controller.getCurrentRoom() == "end_room") {
+        map = map.nextStage;
+        controller = new Controller(map,player,"starter_room");
+      }
     } while (!gameOver);
   }
 }
