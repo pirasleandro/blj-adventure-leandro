831cac079d4ba4ec06ffea24b7b6167aa0a846a4
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-21 08:58:19 +0200

    added Room generators
    simpleRoom() -> 1x1 room
    hallwayHorizontal() -> 2x1
    hallwayVertical() -> 1x2
    bigRoom() -> 2x2

--- ASCIIart.java
+++ ASCIIart.java
@@ -49,4 +49,54 @@ public class ASCIIart {
       ok = scan.nextLine();
     }
   }
+
+  public static String simpleRoom(String up, String upDesc, String left, String leftDesc, String right, String rightDesc, String down, String downDesc) {
+    return (
+      "┌───" + up + "───┐\n" +
+      "│   " + upDesc + "   │\n" +
+      left + " " + leftDesc + " " + rightDesc + " " + right + "\n" +
+      "│   " + downDesc + "   │\n" +
+      "└───" + down + "───┘"
+    );
+  }
+
+  public static String hallwayHorizontal(String up, String upDesc, String up2, String upDesc2, String left, String leftDesc, String right, String rightDesc, String down, String downDesc, String down2, String downDesc2) {
+    return (
+      "┌───" + up + "────────" + up2 + "───┐\n" +
+      "│   " + upDesc + "        " + upDesc2 + "   │\n" +
+      left + " " + leftDesc + "            " + rightDesc + " " + right + "\n" +
+      "│   " + downDesc + "        " + downDesc2 + "   │\n" +
+      "└───" + down + "────────" + down2 + "───┘"
+    );
+  }
+
+  public static String hallwayVertical(String up, String upDesc, String left, String leftDesc, String left2, String leftDesc2, String right, String rightDesc, String right2, String rightDesc2, String down, String downDesc) {
+    return (
+      "┌───" + up + "───┐\n" +
+      "│   " + upDesc + "   │\n" +
+      left + " " + leftDesc + " " + rightDesc + " " + right + "\n" +
+      "│         │\n" +
+      "│         │\n" +
+      "│         │\n" +
+      "│         │\n" +
+      left2 + " " + leftDesc2 + " " + rightDesc2 + " " + right2 + "\n" +
+      "│   " + downDesc + "   │\n" +
+      "└───" + down + "───┘"
+    );
+  }
+
+  public static String bigRoom(String up, String upDesc, String up2, String upDesc2, String left, String leftDesc, String left2, String leftDesc2, String right, String rightDesc, String right2, String rightDesc2, String down, String downDesc, String down2, String downDesc2) {
+    return (
+      "┌───" + up + "────────" + up2 + "───┐\n" + 
+      "│   " + upDesc + "        " + upDesc2 + "   │\n" +
+      left + " " + leftDesc + "            " + rightDesc + " " + right + "\n" +
+      "│                    │\n" +
+      "│                    │\n" +
+      "│                    │\n" +
+      "│                    │\n" +
+      left2 + " " + leftDesc2 + "            " + rightDesc2 + " " + right2 + "\n" +
+      "│   " + downDesc + "        " + downDesc2 + "   │\n" +
+      "└───" + down + "────────" + down2 + "───┘"
+    );
+  }
 }
