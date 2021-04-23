67a17758c7ca48bde195a388586ed9c0c2469452
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-23 11:28:58 +0200

    added helpermethods in Ref class
    now it's possible to get a Room/Door/Item reference without referencing the map/player first

--- Ref.java
+++ Ref.java
@@ -12,4 +12,30 @@ public class Ref {
     controller = controllerRef;
     player = playerRef;
   }
+
+  public static Room getRoom(String id) {
+    return map.getRoom(id);
+  }
+
+  public static String currentRoomId() {
+    return controller.getCurrentRoomId();
+  }
+
+  public static Door getDoor(String id) {
+    return map.getDoor(id);
+  }
+
+  public static Item getItem(String id) {
+    Item output = null;
+    if (player.getItem(id) != null) { // search in player inventory
+      output = player.getItem(id);
+    } else {
+      for (Room room : map.rooms) {   // search in rooms
+        if (room.getItem(id) != null) {
+          output = room.getItem(id);
+        }
+      }
+    }
+    return output;
+  }
 }
