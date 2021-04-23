568041f8eebfb307189a314a2b42087220c51f9a
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-23 08:40:27 +0200

    keycard can now open multiple specific doors

--- Keycard.java
+++ Keycard.java
@@ -1,17 +1,19 @@
 public class Keycard extends Item {
-  public String door_id;
+  public String[] door_id;
 
-  public Keycard(String id, String name, String description, String door_id) {
+  public Keycard(String id, String name, String description, String... door_id) {
     super(id, name, description);
     this.door_id = door_id;
   }
 
   @Override
   void use(String currentRoomId) {
-    Door door = map.getDoor(door_id);
-    Room room = map.getRoom(currentRoomId);
-    if (door.roomId1 == room.id || door.roomId2 == room.id) {
-      door.isLocked = !door.isLocked;
+    for (int i = 0; i < door_id.length; i++) {
+      Door door = map.getDoor(door_id[i]);
+      Room room = map.getRoom(currentRoomId);
+      if (door.roomId1 == room.id || door.roomId2 == room.id) {
+        door.isLocked = !door.isLocked;
+      }
     }
   }
 
