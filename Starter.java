eddb40e6a96e8a34180724b9903e8aaf0b4d3407
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-23 08:45:34 +0200

    added try-catch if nextStage is null
    if a map has no next stage it wont return an error now

--- Starter.java
+++ Starter.java
@@ -1,22 +1,30 @@
+import java.util.Random;
+
 public class Starter {
   public static boolean gameOver = false;
   public static int mapStage = 1;
   public static void main(String[] args) {
-    Map map = MapArchive.ENTRANCE_ZONE;
+    Map map = MapArchive.LIGHT_CONTAINMENT;
     Player player = new Player("testing");
     Controller controller = new Controller(map,player,"starter_room");
     Item.player = controller.getPlayer();
     Item.map = controller.getMap();
+    Debug.controller = controller;
     do {
       ASCIIart.clearConsole();
-      
       controller.printCurrentRoom();
       controller.getInput();
       if (controller.getCurrentRoom() == "end_room") {
-        map = map.nextStage;
-        controller = new Controller(map,player,"starter_room");
-        Item.player = controller.getPlayer();
-        Item.map = controller.getMap();
+        try {
+          map = map.nextStage;
+          controller = new Controller(map,player,"starter_room");
+          Item.player = controller.getPlayer();
+          Item.map = controller.getMap();
+        } catch(Exception e) {
+          System.out.println("No next stage found.");
+          ASCIIart.okToContinue();
+          gameOver = true;
+        }
       }
     } while (!gameOver);
   }
