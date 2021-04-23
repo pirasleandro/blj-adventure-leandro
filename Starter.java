5c558ef009e0f1643b94ad7d2824dbbe39ae30e7
data-corrupted <leandro.piras@noseryoung.ch>
2021-04-23 10:07:13 +0200

    bugfix
    exchanged "==" argument with .equals() so it also switches to next stage if end_room is acessed by debug command

--- Starter.java
+++ Starter.java
@@ -1,28 +1,25 @@
-import java.util.Random;
-
 public class Starter {
   public static boolean gameOver = false;
   public static int mapStage = 1;
   public static void main(String[] args) {
-    Map map = MapArchive.LIGHT_CONTAINMENT;
+    Map map = MapArchive.ENTRANCE_ZONE;
     Player player = new Player("testing");
     Controller controller = new Controller(map,player,"starter_room");
     Item.player = controller.getPlayer();
     Item.map = controller.getMap();
     Debug.controller = controller;
     do {
-      ASCIIart.clearConsole();
+      ConsoleUtil.clear();
       controller.printCurrentRoom();
       controller.getInput();
-      if (controller.getCurrentRoom() == "end_room") {
+      if (controller.getCurrentRoom().equals("end_room")) {
         try {
           map = map.nextStage;
           controller = new Controller(map,player,"starter_room");
           Item.player = controller.getPlayer();
           Item.map = controller.getMap();
         } catch(Exception e) {
-          System.out.println("No next stage found.");
-          ASCIIart.okToContinue();
+          ConsoleUtil.cToClose("No next stage found.");
           gameOver = true;
         }
       }
